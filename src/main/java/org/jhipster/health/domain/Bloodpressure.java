75d424f632010614c10844abc177bb7f699b5f49
Kenneth Boh <kennethboh@streamcastasia.com>
2017-03-01 07:04:13 +0000

    Commit

---
+++ src/main/java/org/jhipster/health/domain/Bloodpressure.java
@@ -0,0 +1,131 @@
+package org.jhipster.health.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * A Bloodpressure.
+ */
+@Entity
+@Table(name = "bloodpressure")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Document(indexName = "bloodpressure")
+public class Bloodpressure implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Column(name = "date", nullable = false)
+    private LocalDate date;
+
+    @Column(name = "systolic")
+    private Integer systolic;
+
+    @Column(name = "diastolic")
+    private Integer diastolic;
+
+    @ManyToOne
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Bloodpressure date(LocalDate date) {
+        this.date = date;
+        return this;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getSystolic() {
+        return systolic;
+    }
+
+    public Bloodpressure systolic(Integer systolic) {
+        this.systolic = systolic;
+        return this;
+    }
+
+    public void setSystolic(Integer systolic) {
+        this.systolic = systolic;
+    }
+
+    public Integer getDiastolic() {
+        return diastolic;
+    }
+
+    public Bloodpressure diastolic(Integer diastolic) {
+        this.diastolic = diastolic;
+        return this;
+    }
+
+    public void setDiastolic(Integer diastolic) {
+        this.diastolic = diastolic;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public Bloodpressure user(User user) {
+        this.user = user;
+        return this;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bloodpressure bloodpressure = (Bloodpressure) o;
+        if (bloodpressure.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, bloodpressure.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Bloodpressure{" +
+            "id=" + id +
+            ", date='" + date + "'" +
+            ", systolic='" + systolic + "'" +
+            ", diastolic='" + diastolic + "'" +
+            '}';
+    }
+}
